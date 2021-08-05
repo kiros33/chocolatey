a3dda05096ae146bcdcd757145800229c75eaece
Sangyong Bae <sybae@zionex.com>
2021-08-05 15:12:14 +0900

    radis-64

--- redis-64/tools/chocolateyinstall.ps1
+++ redis-64/tools/chocolateyinstall.ps1
@@ -16,6 +16,7 @@ $packageArgs = @{
   fileType      = 'msi' #only one of these: exe, msi, msu
   file         = $fileLocation
   softwareName  = 'redis-64*'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes= @(0, 3010, 1641)
 }
 
